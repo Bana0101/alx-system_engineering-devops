cfc6df3bc51cd7955056f20ccda4728d067737ce
ayoub <sb.naba2@gmail.com>
2024-02-27 16:51:09 +0100

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,2 @@
 #!/usr/bin/env bash
-# 100-puppet_ssh_config.pp
-
-file { 'etc/ssh/ssh_config':
-  ensure => present,
-}
-
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
-}
-
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile',
-}
-
-
-      
+# task 100
