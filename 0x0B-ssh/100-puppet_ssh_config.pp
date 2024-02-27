6061b8fb7466360ff4c21030199771988d79274c
ayoub <sb.naba2@gmail.com>
2024-02-27 16:35:25 +0100

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-# use ssh to connect to our server using private key
-file { 'etc/ssh/ssh-config':
-  ensure => file,
-  mode => '0600',
-  content => "
-    Host 100.25.205.20
-      User ubuntu
-      IdentityFile ~/.ssh/school
-      PasswordAuthentication no
-  ",
+# 100-puppet_ssh_config.pp
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/sshd_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
 }
 
 
