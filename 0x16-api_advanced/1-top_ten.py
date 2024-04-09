d5b33ae688e057728c5b282db58c0f8e0b888f04
ayoub <sb.naba2@gmail.com>
2024-04-09 16:49:36 +0000

    prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""  requests module """
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """ a function that queries the Reddit API
-    and returns the number of subscribers """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    response = requests.get(url, allow_redirects=False)
-    if response.status_code >= 300:
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         print('None')
     else:
-        results = response.json().get("data").get("children")
-        [print(child.get("data").get("title")) for child in results]
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
