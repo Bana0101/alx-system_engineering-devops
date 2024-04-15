e3c8f0134d0db5fd25f27d7fc3f4d63a66c1babc
ayoub <sb.naba2@gmail.com>
2024-04-15 22:34:56 +0100

    test

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
+# debugging a wordpress website running on a lamp stack 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
